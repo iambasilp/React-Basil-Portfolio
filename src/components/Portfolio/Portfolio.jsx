77208a135c8719fc10fe9ec32443210568ba80cd
iambasilp <basilxg321@gmail.com>
2023-01-11 21:03:46 +0530

    Portfolio processing ....

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -3,7 +3,16 @@ import './portfolio.css'
 const Portfolio = () => {
   return (
     <section className="portfolio section" id="section">
-      
+      <h5>My Recent Works</h5>
+      <h2>Portfolio</h2>
+      <div className="portfolio__container container">
+        <article className='portfolio__item'>
+          <div className="portfolio-item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn'></a>
+          <a href="https://github.com" className='btn btn-primary'></a>
+        </article>
+      </div>
     </section>
   )
 }
