48ac2363ba947223792f3eedb0495f30d37c7091
iambasilp <basilxg321@gmail.com>
2023-01-11 13:28:02 +0530

    Experience section added

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,11 +1,94 @@
-import React from 'react'
-import './experience.css'
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
   return (
     <section className="experience section" id="experience">
-      
-    </section>
-  )
-}
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+      <div className="experience__container container grid">
+        <div className="experience__frontend ">
+          <h3>Frontend Development</h3>
+          <div className="experience__card">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                
+                <h4>HTML</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                
+                <h4>CSS</h4>
+                <small className="text-light">Intermediate</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
 
-export default Experience
+                <h4>Javascript</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+
+                <h4>Bootstrap</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+
+                <h4>Tailwind</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+
+                <h4>React</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend ">
+          <h3>Backend Development</h3>
+          <div className="experience__card">
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>Node Js</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                  <h4>Express</h4>
+                  <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
+                <h4>MongoDB</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experience;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -3,7 +3,7 @@ import './services.css'
 const Services = () => {
   return (
     <section className="services section" id="services">
-      <h1>Services Section here...</h1>
+      
     </section>
   )
 }
