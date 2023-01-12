94616c3ada7407a2da2516ed08e28bbfbecdeb1d
iambasilp <basilxg321@gmail.com>
2023-01-12 22:11:41 +0530

    Tesimonial section finished

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,6 +4,16 @@ import AVTR1 from "../../assets/avatar1.jpg";
 import AVTR2 from "../../assets/avatar2.jpg";
 import AVTR3 from "../../assets/avatar3.jpg";
 import AVTR4 from "../../assets/avatar4.jpg";
+
+// import Swiper core and required modules
+import { Pagination } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
 const ClientData = [
   {
     id: 1,
@@ -14,7 +24,7 @@ const ClientData = [
               ex, cum consequuntur dolore a et illum inventore, dolorem voluptas`,
   },
   {
-    id: 1,
+    id: 2,
     Image: AVTR2,
     Name: "Ayas Muhammed",
     Review: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
@@ -22,7 +32,7 @@ const ClientData = [
               ex, cum consequuntur dolore a et illum inventore, dolorem voluptas`,
   },
   {
-    id: 1,
+    id: 3,
     Image: AVTR3,
     Name: "Krishna",
     Review: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
@@ -30,7 +40,7 @@ const ClientData = [
               ex, cum consequuntur dolore a et illum inventore, dolorem voluptas`,
   },
   {
-    id: 1,
+    id: 4,
     Image: AVTR4,
     Name: "Jordan Loake",
     Review: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
@@ -38,22 +48,29 @@ const ClientData = [
               ex, cum consequuntur dolore a et illum inventore, dolorem voluptas`,
   },
 ];
+
 const Testimonials = () => {
   return (
     <section className="testimonials section" id="testimonials">
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
-      <div className="testimonials__container container">
-        {ClientData.map(({ id, Image, Name, Review }) => (
-          <article key={id} className="testimonials__card">
-            <div className="client">
-              <img src={Image} alt="" />
-            </div>
+      <Swiper  pagination={{ clickable: true }} modules={[Pagination]} spaceBetween={40} slidesPerView={1} className="testimonials__container container">
+        
+        {ClientData.map(({ id, Image, Name, Review }) => {
+          return (
+            <SwiperSlide key={id} className="testimonials__card">
+              <div className="client">
+                <img src={Image} alt="" />
+              </div>
               <h5 className="client__name">{Name}</h5>
               <small className="client__review">{Review}</small>
-          </article>
-        ))}
-      </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+      <br />
+      <br />
+      <br />
     </section>
   );
 };
