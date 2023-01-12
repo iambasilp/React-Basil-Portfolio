ccc6ad201decc52cc177753618808e94ce1b9110
iambasilp <basilxg321@gmail.com>
2023-01-12 20:44:55 +0530

    Portfolio Section finished

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,20 +1,85 @@
-import React from 'react'
-import './portfolio.css'
+import React from "react";
+import "./portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
 const Portfolio = () => {
+  const PortfolioData = [
+    {
+      id: 1,
+      photo: IMG1,
+      title: "Crypte Currency Dashboard & Financial Visualization",
+      github: "https://github.com",
+      demo: "https://cdn.dribbble.com/users/1998175/screenshots/16766927/media/bb23da53a260e2af2e9730fce44ab684.jpg?compress=1&resize=768x576&vertical=top",
+    },
+    {
+      id: 2,
+      photo: IMG2,
+      title: "Crypte Currency Dashboard & Financial Visualization",
+      github: "https://github.com",
+      demo: "https://cdn.dribbble.com/users/1998175/screenshots/16766927/media/bb23da53a260e2af2e9730fce44ab684.jpg?compress=1&resize=768x576&vertical=top",
+    },
+    {
+      id: 3,
+      photo: IMG3,
+      title: "Crypte Currency Dashboard & Financial Visualization",
+      github: "https://github.com",
+      demo: "https://cdn.dribbble.com/users/1998175/screenshots/16766927/media/bb23da53a260e2af2e9730fce44ab684.jpg?compress=1&resize=768x576&vertical=top",
+    },
+    {
+      id: 4,
+      photo: IMG4,
+      title: "Crypte Currency Dashboard & Financial Visualization",
+      github: "https://github.com",
+      demo: "https://cdn.dribbble.com/users/1998175/screenshots/16766927/media/bb23da53a260e2af2e9730fce44ab684.jpg?compress=1&resize=768x576&vertical=top",
+    },
+    {
+      id: 5,
+      photo: IMG5,
+      title: "Crypte Currency Dashboard & Financial Visualization",
+      github: "https://github.com",
+      demo: "https://cdn.dribbble.com/users/1998175/screenshots/16766927/media/bb23da53a260e2af2e9730fce44ab684.jpg?compress=1&resize=768x576&vertical=top",
+    },
+    {
+      id: 6,
+      photo: IMG6,
+      title: "Crypte Currency Dashboard & Financial Visualization",
+      github: "https://github.com",
+      demo: "https://cdn.dribbble.com/users/1998175/screenshots/16766927/media/bb23da53a260e2af2e9730fce44ab684.jpg?compress=1&resize=768x576&vertical=top",
+    },
+  ];
+
   return (
     <section className="portfolio section" id="section">
       <h5>My Recent Works</h5>
       <h2>Portfolio</h2>
       <div className="portfolio__container container">
-        <article className='portfolio__item'>
-          <div className="portfolio-item-image"></div>
-          <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn'></a>
-          <a href="https://github.com" className='btn btn-primary'></a>
-        </article>
+        {PortfolioData.map(({ id, photo, title, github, demo }) => (
+          <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img
+                src={photo}
+                alt="portfolio image"
+                className="portfolio__img"
+              />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__buttons">
+              <a href={github} className="btn" target="_blank">
+                github
+              </a>
+              <a href={demo} className="btn btn-primary" target="_blank">
+                Live Demo
+              </a>
+            </div>
+          </article>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
