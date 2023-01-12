28fe7a8884cf92e659b8a457141f3d826bbac051
iambasilp <basilxg321@gmail.com>
2023-01-12 22:27:49 +0530

    Contact section is  processing ....

--- src/components/contacts/Contact.jsx
+++ src/components/contacts/Contact.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 const Contact = () => {
   return (
-   <section className="contact section" id="contact">
-    
-   </section>
-  )
-}
+    <section className="contact section" id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
 
-export default Contact
+      <div className="contact__container container grid">
+        <div className="contact__data grid">
+          <article className="contact__information">
+            <MdOutlineEmail className="contact__inforamtion-icon" />
+            <h4>Email</h4>
+            <h5>basilxg321@gmail.com</h5>
+            <a href="mailto:basilxg321@gmail.com">Send a email</a>
+          </article>
+          <article className="contact__information">
+            <RiMessengerLine className="contact__inforamtion-icon" />
+            <h4>Messenger</h4>
+            <h5>+91 907 439 7747</h5>
+            <a href="https://m.me/iambasilp/">Send a message</a>
+          </article>
+          <article className="contact__information">
+            <BsWhatsapp className="contact__inforamtion-icon" />
+            <h4>Email</h4>
+            <h5>basilxg321@gmail.com</h5>
+            <a href="https://wa.me/919074397747?text=Hello%2C%20Basil%20Pulikuth%20%E2%9C%8B">
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" placeholder="You Email" required />
+          <textarea
+            name=""
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
